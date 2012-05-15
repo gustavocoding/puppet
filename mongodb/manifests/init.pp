151d0eb0fea6fa69eafb7c9382c6be8ae53c1e4d
gustavonalle <gustavonalle@gmail.com>
2012-05-15 20:02:36 +0100

    Permission fixing

--- mongodb/manifests/init.pp
+++ mongodb/manifests/init.pp
@@ -56,14 +56,14 @@ class mongodb($replicaset = "undefined", $numa = "undefined") {
    
         exec {
                "download-mongo" :
-               command  => "/usr/bin/wget http://downloads.mongodb.org/linux/${mongo_tgz} -N -P $base_dir/",
+               command  => "/usr/bin/wget http://downloads.mongodb.org/linux/${mongo_tgz} -N -P $base_dir/ && chown -R mongodb.mongodb $base_dir",
                alias => "mongodb-source-tgz",
                before => Exec["untar-mongo"],
                require => File["mongo-base"],
         }
 
 	exec { "untar ${mongo_tgz}":
-                command => "/bin/tar -zxf ${mongo_tgz}",
+                command => "/bin/tar -zxf ${mongo_tgz} && chown -R mongodb.mongodb $base_dir",
                 cwd => "$base_dir",
                 creates => "$base_dir/mongodb",
                 alias => "untar-mongo",
