466b269688c69651d3479abac9a893b30efd8d8a
Gustavo Fernandes <gustavonalle@gmail.com>
2013-02-04 18:17:52 +0000

    Refactorings

--- mongodb/manifests/init.pp
+++ mongodb/manifests/init.pp
@@ -1,104 +1,104 @@
-# /etc/puppet/modules/mongodb/manifests/init.pp
+class mongodb($primary,$secondaries,$arbiter,$replicaset = "undefined", $disablenuma = "undefined") {
 
-class mongodb($replicaset = "undefined", $disablenuma = "undefined") {
+  	$version = "2.2.2"
+        $arch =  "linux-x86_64"
+        $user = "mongodb"
+	$group = "mongodb"
+        $basedir = "/opt/mongodb"
+        $datadir = "/datadb/mongodb"
 
-        require mongodb::params
+        $installdir = "${basedir}/mongodb-$arch-$version"
+	$mongo_tgz = "mongodb-${arch}-${version}.tgz"
 
-	$mongo_tgz = "mongodb-${mongodb::params::arch}-${mongodb::params::version}.tgz"
-        $base_dir = "${mongodb::params::base}"
-        $data_dir = "${mongodb::params::data}"
+	$log_path = "$datadir/logs/mongo.log"
 
-	$log_path = "$data_dir/logs/mongo.log"
-
-        group { "mongodb":
+        group { "$group":
         	ensure  => present
 	}
 
-	user{ "mongodb": 
+	user{ "$user": 
         	ensure  => present, 
-                gid   => "mongodb",
+                gid   => "$group",
         	shell   => "/sbin/nologin" 
 	} 
+
+	$basedirs = ["$basedir", "$installdir", "$installdir/conf"]
 	
-        file { "$base_dir":
+        file { $basedirs:
 		ensure => "directory",
-		owner => "mongodb",
-		group => "mongodb",
-		alias => "mongo-base"
+		owner => "$user",
+		group => "$group",
+		require => [ User["$user"], Group["$group"] ]
 	}
 
-        file { "$base_dir/conf":
-               ensure => "directory",
-               owner => "mongodb",
-               group => "mongodb",
-               alias => "mongo-conf",
-                require => File[$base_dir]
-        }
 
-        file { "$base_dir/conf/mongo.conf":
+        file { "$installdir/conf/mongo.conf":
                 mode => 0744,
                 ensure => present,
-                content => template('mongodb/mongodb.conf.erb')
+		alias => "mongodb-config-file",
+                content => template('mongodb/mongodb.conf.erb'),
+        }
+
+        file { "$installdir/bin/create-replica.sh":
+                mode => 0755,
+		owner => "$user",
+		group => "$group",
+                ensure => present,
+		require => [Exec["untar-mongo"]],
+                content => template('mongodb/create-replica.erb'),
         }
         
-       file { "/data": 
-		ensure => "directory",
-		owner => "mongodb",
-		group => "mongodb",
-                alias => "data-root"
-	}
+        $datadirs = [ "/datadb", "$datadir", "$datadir/logs" ]
 
-        file { "$data_dir": 
+        file { $datadirs: 
 		ensure => "directory",
-		owner => "mongodb",
-		group => "mongodb",
-                alias => "mongo-data",
-                require => File["data-root"]
-	}
-
-        file { "$data_dir/logs": 
-		ensure => "directory",
-		owner => "mongodb",
-		group => "mongodb",
-                alias => "mongo-logs"
-	}
-
-        file { "$data_dir/logs/mongo.log":
-		ensure => "present",
-		owner => "mongodb",
-		group => "mongodb",
-		alias => "mongo-logfile"
-	}
-   
-        exec {
-               "download-mongo" :
-               command  => "/usr/bin/wget http://downloads.mongodb.org/linux/${mongo_tgz} -N -P $base_dir/ && chown -R mongodb.mongodb $base_dir",
-               alias => "mongodb-source-tgz",
-               before => Exec["untar-mongo"],
-               require => File["mongo-base"],
+		owner => "$user",
+		group => "$group",
         }
 
+        file { "$datadir/logs/mongo.log":
+		ensure => "present",
+		owner => "$user",
+		group => "$group",
+		alias => "mongo-logfile",
+		require => File[$datadirs]
+        }
+   
+	file { "${basedir}/${mongo_tgz}" :
+		mode   => 0744,
+		ensure => present,
+		owner  => "$user",
+		group  => "$group",
+		alias  => "mongo_tgz",
+                before => Exec["untar-mongo"],
+		source => "puppet:///modules/mongodb/${mongo_tgz}",
+                require => File[$basedirs],
+	}
+
 	exec { "untar ${mongo_tgz}":
-                command => "/bin/tar -zxf ${mongo_tgz} && chown -R mongodb.mongodb $base_dir",
-                cwd => "$base_dir",
-                creates => "$base_dir/mongodb",
+                command => "/bin/tar xvf ${basedir}/${mongo_tgz}  && chown -R $user.$group $basedir",
+		cwd => $basedir,
                 alias => "untar-mongo",
                 refreshonly => true,
-                subscribe => Exec["mongodb-source-tgz"],
+                subscribe => File["mongo_tgz"],
         }
       
         file { "/etc/init.d/mongodb":
                 mode => 0744,
                 ensure => present,
+		alias  => "init-script",
                 content => template('mongodb/mongodb.erb')
-       }
+        }
 
         file { "/etc/profile.d/set_mongo_path.sh":
                 ensure => present,
+		alias  => "mongo_path",
                 content => template('mongodb/set_mongo_path.erb')
-       }
+        }
 
-
-
+	service { "mongodb":
+		ensure  => running,
+		require => [File["mongodb-config-file"],File["mongo_path"],Exec["untar-mongo"],File[$basedirs],File[$datadirs],File["init-script"]]
+	}
 
 }
